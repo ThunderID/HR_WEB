f547d3b6022ddcaec03ae6114c2bdcc3fa25570f
budi_p <budi-purnomo@outlook.com>
2015-04-06 16:58:51 +0700

    patching company branch add

--- resources/views/admin/pages/organisation/organisation-branch/index.blade.php
+++ resources/views/admin/pages/organisation/organisation-branch/index.blade.php
@@ -8,7 +8,11 @@
 @section('content')
 	<div class="row pb-10">
 		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<a href="{{route('admin.company.add') }}" class='btn btn-raised btn-primary ink-reaction mt-10'>Create New</a>
+			@if($controller_name == "department")
+			<a href="{{route('admin.department.add') }}" class='btn btn-raised btn-primary ink-reaction mt-10'>Create New</a>
+			@elseif($controller_name == "position")
+			<a href="{{route('admin.position.add') }}" class='btn btn-raised btn-primary ink-reaction mt-10'>Create New</a>
+			@endif
 		</div>
 		<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 form pull-right">
 			{!! Form::open(['route' => null, 'class' => 'form-inline']) !!}
