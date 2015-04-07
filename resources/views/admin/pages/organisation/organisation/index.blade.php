a30419bc7171ca150a641e62533c89c9e34d4930
budi_p <budi-purnomo@outlook.com>
2015-04-07 09:42:10 +0700

    patching organisationn index

--- resources/views/admin/pages/organisation/organisation/index.blade.php
+++ resources/views/admin/pages/organisation/organisation/index.blade.php
@@ -17,8 +17,40 @@
 				<button class="btn btn-raised btn-default-light ink-reaction" type="submit">Search</button>
 			{!! Form::close() !!}
 		</div>
-		
 	</div>
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="card">
+				<div class="card-body">
+					<table class="table no-margin">
+						<thead>
+							<tr>
+								<th>No</th>
+								<th>Nama Organisasi</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>Mentari Pagi Sejahtera</td>
+								<td>
+									
+								</td>
+							</tr>
+							<tr>
+								<td>2</td>
+								<td>Bintang Malam Senyap</td>
+								<td>
+									
+								</td>
+							</tr>							
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>	
 @stop
 
 
