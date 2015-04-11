1a63afe0f9f953e0ed90fd8b279700bcac549a4b
budi_p <budi-purnomo@outlook.com>
2015-04-11 14:43:50 +0700

    patching companies show

--- resources/views/admin/pages/organisation/company/show.blade.php
+++ resources/views/admin/pages/organisation/company/show.blade.php
@@ -91,9 +91,55 @@
 							</div>
 						</div>							
 					</div>
-					<div class="tab-pane" id="Document">
-						<h3 class="text-light">Dokumen Perusahaan</h3>
-						<p>Quod option numquam vel in, et fuisset delicatissimi duo, qui ut animal noluisse erroribus. Ea eum veniam audire. Per at postea mediocritatem, vim numquam aliquid eu, in nam sale gubergren. Dicant vituperata consequuntur at sea, mazim commodo</p>
+						<div class="col-md-12 tab-pane" id="Document">
+							<ul class="card-head nav nav-tabs text-center" data-toggle="tabs">
+								<li class="active">
+									<a href="#data_document">
+										<i class="fa fa-lg fa-file-text-o"></i>
+										<br/>
+										<h4>
+											Data<br/>
+										</h4>
+									</a>
+								</li>
+								<li>
+									<a href="#add_document">
+										<i class="fa fa-lg fa-plus"></i>
+										<br/>
+										<h4>
+											Tambah<br/>
+										</h4>
+									</a>
+								</li>
+							</ul>
+							<div class="card-body tab-content style-default-bright">
+								<div class="tab-pane active" id="data_document">
+									<h3 class="text-light">Dokumen Perusahaan</h3>
+									<br>
+									<p class="lead">Ad ius duis dissentiunt, an sit harum primis persecuti, adipisci tacimates mediocrem sit et. Id illud voluptaria omittantur qui, te affert nostro mel. Cu conceptam vituperata temporibus has.</p>
+									<p>Per at postea mediocritatem, vim numquam aliquid eu, in nam sale gubergren. Fuisset delicatissimi duo, qui ut animal noluisse erroribus. Ea eum veniam audire. Dicant vituperata consequuntur.</p>
+									<br>
+									<p class="lead">Ad ius duis dissentiunt, an sit harum primis persecuti, adipisci tacimates mediocrem sit et. Id illud voluptaria omittantur qui, te affert nostro mel. Cu conceptam vituperata temporibus has.</p>
+									<p>Per at postea mediocritatem, vim numquam aliquid eu, in nam sale gubergren. Fuisset delicatissimi duo, qui ut animal noluisse erroribus. Ea eum veniam audire. Dicant vituperata consequuntur.</p>
+								</div>
+
+								<div class="tab-pane" id="add_document">
+
+								<div class="col-md-12">
+									<br>
+									<form class="form" role="form">
+										<div class="form-group floating-label">
+											<input type="text" class="form-control" id="docs_name">
+											<label for="docs_name">Nama Dokumen</label>
+										</div>
+									</form>
+								</div><!--end .col -->	
+
+								<div class="card-actionbar-row">
+									<button type="submit" class="btn btn-flat btn-accent">SIMPAN DATA</button>
+								</div><!--end .card-actionbar-row -->
+							</div>				
+						</div><!--end .card -->
 					</div>
 				</div><!--end .card-body -->
 			</div><!--end .card -->
