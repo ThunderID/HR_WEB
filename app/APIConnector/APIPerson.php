6bbcce8a1a24f1bc9c27d76834e28c4d4a0c6900
chelsymooy <chelsymooy1108@gmail.com>
2015-04-14 12:59:12 +0700

    updating apiperson

--- app/APIConnector/APIPerson.php
+++ app/APIConnector/APIPerson.php
@@ -35,4 +35,34 @@ class APIPerson {
 	{
 		return self::runGet(self::$basic_url . 'persons/delete/'.$id, ['id' => $id]);
 	}
+
+	function documentIndex($personid, $page, $search, $sort)
+	{
+		return self::runPost(self::$basic_url .  '/persons/'.$personid.'/documents/'.$page, ['search' => $search, 'sort' => $sort]);
+	}
+
+	function documentShow($personid,$id)
+	{
+		return self::runGet(self::$basic_url . '/persons/'.$personid.'/documents/show/'.$id, ['id' => $id]);
+	}
+
+	function documentDestroy($personid,$id)
+	{
+		return self::runGet(self::$basic_url . '/persons/'.$personid.'/documents/delete/'.$id, ['id' => $id]);
+	}
+
+	function relativeDestroy($personid,$id)
+	{
+		return self::runGet(self::$basic_url . '/persons/'.$personid.'/relatives/delete/'.$id, ['id' => $id]);
+	}
+
+	function workIndex($personid, $page, $search, $sort)
+	{
+		return self::runPost(self::$basic_url .  '/persons/'.$personid.'/works/'.$page, ['search' => $search, 'sort' => $sort]);
+	}
+
+	function workShow($personid,$id)
+	{
+		return self::runGet(self::$basic_url . '/persons/'.$personid.'/works/show/'.$id, ['id' => $id]);
+	}
 }
