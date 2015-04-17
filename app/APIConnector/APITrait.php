f7482a76f6670d2da40a092332aea03719a5c70c
chelsymooy <chelsymooy1108@gmail.com>
2015-04-17 20:17:48 +0700

    error handling

--- app/APIConnector/APITrait.php
+++ app/APIConnector/APITrait.php
@@ -1,11 +1,22 @@
 <?php namespace App\APIConnector;
 
+use Exception;
+
 trait APITrait {
 
-	static protected $basic_url = 'http://localhost:9200/';
+	static protected $basic_url = 'http://localhost:7200/';
 
 	protected static function runGet($url, $data = [])
 	{
+		try
+		{
+			fsockopen('localhost', '7200', $errno, $errstr, 60);
+		}
+		catch (Exception $e) 
+		{
+			throw new Exception("Running API dulu gan!! di port 7200.. (Udah ada warnnya nih ©thunder)", 1);
+		}
+
 		$curl 			= curl_init($url);
 		curl_setopt($curl, CURLOPT_HEADER, false);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -15,11 +26,25 @@ trait APITrait {
 
 		$results 		= curl_exec($curl);
 
+		if(!json_decode($results))
+		{
+			print_r($results);
+		}
+
 		return $results;
 	}
 
 	protected static function runPost($url, $data = [])
 	{
+		try
+		{
+			fsockopen('localhost', '7200', $errno, $errstr, 60);
+		}
+		catch (Exception $e) 
+		{
+			throw new Exception("Running API dulu gan!! di port 7200.. (Udah ada warnnya nih ©thunder)", 1);
+		}
+
 		$content 		= json_encode($data);
 
 		$curl 			= curl_init($url);
@@ -32,6 +57,11 @@ trait APITrait {
 		curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 
 		$results 		= curl_exec($curl);
+print_r($results);exit;
+		if(!json_decode($results))
+		{
+			print_r($results);
+		}
 
 		return $results;
 	}
