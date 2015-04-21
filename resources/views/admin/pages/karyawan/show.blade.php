c2c9430f3ec1488dc065dc389a84f455ab27a1f1
budi_p <budi-purnomo@outlook.com>
2015-04-21 14:34:55 +0700

    patching Show Karyawan #6 #18 : Konfirmasi delete (check password)

--- resources/views/admin/pages/karyawan/show.blade.php
+++ resources/views/admin/pages/karyawan/show.blade.php
@@ -12,7 +12,7 @@
 				<a href="{{route('hr.persons.index')}}" class="btn btn-flat ink-reaction pull-left">
 					<i class="md md-reply"></i> Kembali
 				</a>
-				<a href="{{route('hr.persons.delete', [$data['id']])}}" class="btn btn-flat ink-reaction pull-right">
+				<a class="btn btn-flat ink-reaction pull-right" data-toggle="modal" data-target="#del_modal">
 					<i class="fa fa-trash"></i> Hapus
 				</a>
 				<a href="{{route('hr.persons.edit', [$data['id']])}}" class="btn btn-flat ink-reaction pull-right">
@@ -95,6 +95,37 @@
 			@include('admin.helpers.person-rightbar')
 		</div>
 	</div>
+
+	<div class="modal fade" id="del_modal" tabindex="-1" role="dialog" aria-labelledby="del_modal" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				{!! Form::open(array('route' => array('hr.persons.delete', $data['id']),'method' => 'POST')) !!}
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title" id="simpleModalLabel">Hapus Data Relasi</h4>
+				</div>
+				<div class="modal-body">
+					<p>Apakah Anda yakin akan menghapus data relasi? Silahkan masukkan password Anda untuk konfirmasi.</p>
+					<div class="row">
+						<div class="form-group">
+							<div class="col-sm-3">
+								<label for="password1" class="control-label">Password</label>
+							</div>
+							<div class="col-sm-9">
+								<input type="password" name="password" id="password" class="form-control" placeholder="Password">
+							</div>
+						</div>					
+					</div>
+				</div>
+				<div class="modal-footer">
+					<p>{!! Form::hidden('from_confirm_form', 'Yes') !!}</p>
+					<a type="button" class="btn btn-default" data-dismiss="modal">Cancel</a>
+					<button type="submit" type="button" class="btn btn-danger">Hapus</button>
+				</div>
+				{!! Form::close() !!}
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div>	
 @stop
 
 @section('css')
