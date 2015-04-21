4ed125457997fdb3de96b76ad4d6d881f227bbe6
budi_p <budi-purnomo@outlook.com>
2015-04-21 18:21:40 +0700

    patching issue Branch #6 #23

--- resources/views/admin/pages/organisation/kantor/struktur/index.blade.php
+++ resources/views/admin/pages/organisation/kantor/struktur/index.blade.php
@@ -1,8 +1,12 @@
+	<div class="card-actionbar">
+		<a class="btn btn-primary pull-right" href="{{route('hr.organisation.charts.create', ['id' => $data['id']]) }}">TAMBAH</a>
+	</div>
+	<div class="clearfix">&nbsp;</div>
 	@foreach($data['charts'] as $key => $value)
 		<div class="row">
 			<div class="col-sm-9">@for($i=1;$i<count(explode(',',$value['path']));$i++)&nbsp;&nbsp;&nbsp;@endfor <i class="fa fa-chevron-circle-right"></i>&nbsp;&nbsp;{{$value['name']}}</div>
 			<div class="text-right col-sm-1">
-				<a href="{{route('hr.organisation.charts.delete', [$data['id'], $value['id']])}}">
+				<a href="#" data-toggle="modal" data-target="#del_modal2">
 					<i class="fa fa-trash"></i>
 				</a>
 			</div>
@@ -18,9 +22,3 @@
 			</div>
 		</div>
 	@endforeach
-	<div class="clearfix">&nbsp;</div>
-	<div class="card-actionbar">
-		<div class="card-actionbar-row">
-			<a class="btn btn-flat" href="{{route('hr.organisation.charts.create', ['id' => $data['id']]) }}">TAMBAH</a>
-		</div>
-	</div>
