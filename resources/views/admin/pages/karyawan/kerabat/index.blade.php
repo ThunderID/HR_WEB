de0b174443e547a042e53270cb52d58b9699d9dd
budi_p <budi-purnomo@outlook.com>
2015-04-22 14:14:02 +0700

    patching delete relasi need password to confirm action

--- resources/views/admin/pages/karyawan/kerabat/index.blade.php
+++ resources/views/admin/pages/karyawan/kerabat/index.blade.php
@@ -118,24 +118,36 @@
 	</div>
 
 	<!-- BEGIN MODAL -->
-
 	<div class="modal fade" id="del_modal" tabindex="-1" role="dialog" aria-labelledby="del_modal" aria-hidden="true">
 		<div class="modal-dialog">
 			<div class="modal-content">
+				{!! Form::open(array('route' => array('hr.persons.relatives.delete', [ $data['id'],$value['relative_id']]),'method' => 'POST')) !!}
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title" id="simpleModalLabel">Hapus Data Relasi</h4>
 				</div>
 				<div class="modal-body">
-					<p>Apakah Anda yakin akan menghapus data relasi?</p>
+					<p>Apakah Anda yakin akan menghapus data relasi? Silahkan masukkan password Anda untuk konfirmasi.</p>
+					<div class="row">
+						<div class="form-group">
+							<div class="col-sm-3">
+								<label for="password1" class="control-label">Password</label>
+							</div>
+							<div class="col-sm-9">
+								<input type="password" name="password" id="password" class="form-control" placeholder="Password">
+							</div>
+						</div>					
+					</div>
 				</div>
 				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-					<a href="{{ route('hr.persons.relatives.delete' ,[ $data['id'],$value['relative_id']]) }}" type="button" class="btn btn-danger">Hapus</a>
+					<p>{!! Form::hidden('from_confirm_form', 'Yes') !!}</p>
+					<a type="button" class="btn btn-default" data-dismiss="modal">Cancel</a>
+					<button type="submit" type="button" class="btn btn-danger">Hapus</button>
 				</div>
+				{!! Form::close() !!}
 			</div><!-- /.modal-content -->
 		</div><!-- /.modal-dialog -->
-	</div>
+	</div>		
 
 	<div class="modal fade" id="add_modal" tabindex="-1" role="dialog" aria-labelledby="add_modal" aria-hidden="true">
 		<div class="modal-dialog modal-lg">
