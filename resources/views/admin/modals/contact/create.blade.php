ba19edcac377421d8fca5bcb67472c32f404f269
budi_p <budi-purnomo@outlook.com>
2015-04-25 13:11:02 +0700

    patching contact modals

--- resources/views/admin/modals/contact/create.blade.php
+++ resources/views/admin/modals/contact/create.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="addressCreate" tabindex="-1" role="dialog" aria-labelledby="addressCreate" aria-hidden="true">
+<div class="modal fade" id="contactCreate" tabindex="-1" role="dialog" aria-labelledby="contactCreate" aria-hidden="true">
 	<div class="modal-dialog">
 		<div class="modal-content">
 			<div class="modal-header text-primary">
@@ -9,7 +9,14 @@
 				<div class="row">
 					<div class="col-md-6">
 						<div class="form-group">
-							<input type="text" class="form-control" id="item[1]" name="item[1]">
+							<select class="form-control" id="item[1]" name="item[1]">
+								<option value=""></option>
+								<option value="phone">Nomor Telepon</option>
+								<option value="email">Email</option>
+								<option value="bbm">BBM</option>
+								<option value="line">Line</option>
+								<option value="whatsapp">WhatsApp</option>
+							</select>							
 							<label for="item[1]">Jenis Kontak</label>
 						</div>
 					</div>
