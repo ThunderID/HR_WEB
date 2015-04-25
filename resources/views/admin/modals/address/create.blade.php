2f2fb4baecacac5a1fe41e0b8761002704a62497
budi_p <budi-purnomo@outlook.com>
2015-04-25 11:36:34 +0700

    patching issue Branch Create & Show #28 #30

---
+++ resources/views/admin/modals/address/create.blade.php
@@ -0,0 +1,38 @@
+<div class="modal fade" id="contactCreate" tabindex="-1" role="dialog" aria-labelledby="contactCreate" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header text-primary">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="simpleModalLabel">Tambah Data Kontak</h4>
+			</div>
+			<div class="modal-body form">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="form-group">
+							<textarea type="email" class="form-control" name="address_address[1]" style="resize: none;" rows="2"></textarea>
+							<label for="address_address[1]">Alamat Lengkap</label>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" class="form-control" id="contact_phone[1]" name="contact_phone[1]">
+							<label for="contact_phone[1]">Nomor Telepon</label>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" class="form-control" id="contact_email[1]" name="contact_email[1]">
+							<label for="contact_email[1]">Alamat Email</label>
+						</div>
+					</div>					
+				</div>				
+			</div>			
+			<div class="modal-footer">
+				<a type="button" class="btn btn-default" data-dismiss="modal">Batal</a>
+				<button type="submit" type="button" class="btn btn-primary">Tambah</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div>	
---
+++ resources/views/admin/modals/contact/create.blade.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="addressCreate" tabindex="-1" role="dialog" aria-labelledby="addressCreate" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header text-primary">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title" id="simpleModalLabel">Tambah Data Kontak</h4>
+			</div>
+			<div class="modal-body form">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" class="form-control" id="item[1]" name="item[1]">
+							<label for="item[1]">Jenis Kontak</label>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group">
+							<input type="text" class="form-control" id="value[1]" name="value[1]">
+							<label for="value[1]">Isian</label>
+						</div>
+					</div>					
+				</div>			
+			</div>			
+			<div class="modal-footer">
+				<a type="button" class="btn btn-default" data-dismiss="modal">Batal</a>
+				<button type="submit" type="button" class="btn btn-primary">Tambah</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div>	
