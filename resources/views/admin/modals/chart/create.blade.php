e4ca7ba4577ca3e31a0bd3d1598cd5b560cd54a1
budi_p <budi-purnomo@outlook.com>
2015-04-28 14:03:25 +0700

    fixed redesign modals dialog

--- resources/views/admin/modals/chart/create.blade.php
+++ resources/views/admin/modals/chart/create.blade.php
@@ -1,69 +1,83 @@
 	<div class="modal-dialog modal-lg form">
-		<div class="modal-content ">
+		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title text-xl" id="formModalLabel">Tambah Struktur Perusahaan</h4>
+				<h4 class="modal-title text-xl" id="formModalLabel">Tambah Relasi</h4>
 			</div>
-			<div class="modal-body">
+			<div class="modal-body style-default-light">
 				<div class="row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<select class="form-control" id="path" name="path">
-								<option value=""></option>
-								@foreach($data['charts'] as $key => $value)
-									<option value="{{$value['path']}}">{{$value['name']}}</option>
-								@endforeach
-							</select>
-							<label for="path">Atasan</label>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">	
-							<input type="text" class="form-control" id="grade" name="tag">
-							<label for="grade">Departemen</label>
-						</div>				
-					</div>				
-				</div>
-				<div class="row">
-					<div class="col-md-12">
-						<div class="form-group">
-							<input type="text" class="form-control input-lg" id="name" name="name">
-							<label for="name">Nama</label>
-						</div>
-					</div>
-				</div><!--end .row -->
-				<div class="row">
-					<div class="col-md-3">
-						<div class="form-group">
-							<input type="text" class="form-control" id="min_employee" name="min_employee">
-							<label for="min_employee">Jumlah Minimum Pegawai</label>
-						</div>
+					<div class="col-lg-12">
+						<h4 class="text-primary">Petunjuk</h4>
+						<article class="margin-bottom-xxl">
+							<p class="opacity-75">
+								Masukkan data sesuai dengan inputan yang ada. 
+							</p>
+						</article>
 					</div><!--end .col -->
-					<div class="col-md-3">
-						<div class="form-group">
-							<input type="text" class="form-control" id="ideal_employee" name="ideal_employee">
-							<label for="ideal_employee">Jumlah Ideal Pegawai</label>
+				</div><!--end .row -->
+				<div class="tabs-left style-default-light">
+					<div class="card-body tab-content style-default-bright">
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<select class="form-control" id="path" name="path">
+										<option value=""></option>
+										@foreach($data['charts'] as $key => $value)
+											<option value="{{$value['path']}}">{{$value['name']}}</option>
+										@endforeach
+									</select>
+									<label for="path">Atasan</label>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">	
+									<input type="text" class="form-control" id="grade" name="tag">
+									<label for="grade">Departemen</label>
+								</div>				
+							</div>				
 						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="form-group">
-							<input type="text" class="form-control" id="max_employee" name="max_employee">
-							<label for="max_employee">Jumlah Maksimum Pegawai</label>
-						</div>
-					</div>
-					<div class="col-md-3">
-						<div class="form-group">
-							<input type="text" class="form-control" id="grade" name="grade" Placehoder="(Biarkan kosong untuk departemen)">
-							<label for="grade">Grade</label>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<input type="text" class="form-control input-lg" id="name" name="name">
+									<label for="name">Nama</label>
+								</div>
+							</div>
+						</div><!--end .row -->
+						<div class="row">
+							<div class="col-md-3">
+								<div class="form-group">
+									<input type="text" class="form-control" id="min_employee" name="min_employee">
+									<label for="min_employee">Jumlah Minimum Pegawai</label>
+								</div>
+							</div><!--end .col -->
+							<div class="col-md-3">
+								<div class="form-group">
+									<input type="text" class="form-control" id="ideal_employee" name="ideal_employee">
+									<label for="ideal_employee">Jumlah Ideal Pegawai</label>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="form-group">
+									<input type="text" class="form-control" id="max_employee" name="max_employee">
+									<label for="max_employee">Jumlah Maksimum Pegawai</label>
+								</div>
+							</div>
+							<div class="col-md-3">
+								<div class="form-group">
+									<input type="text" class="form-control" id="grade" name="grade" Placehoder="(Biarkan kosong untuk departemen)">
+									<label for="grade">Grade</label>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div class="card-actionbar">
-					<div class="card-actionbar-row">
-						<a class="btn btn-flat" data-dismiss="modal" aria-hidden="true">BATAL</a>
-						<button type="submit" class="btn btn-flat btn-accent">SIMPAN DATA</button>
-					</div><!--end .card-actionbar-row -->
-				</div><!--end .card-actionbar -->				
 			</div>
+			<div class="card-actionbar style-default-light">
+				<div class="card-actionbar-row">
+					<a class="btn btn-flat" data-dismiss="modal" aria-hidden="true">BATAL</a>
+					<button type="submit" class="btn btn-flat btn-primary">SIMPAN DATA</button>
+				</div><!--end .card-actionbar-row -->
+			</div><!--end .card-actionbar -->				
 		</div>
 	</div>
--- resources/views/admin/modals/delete/delete.blade.php
+++ resources/views/admin/modals/delete/delete.blade.php
@@ -1,11 +1,18 @@
-	<div class="modal-dialog">
+	<div class="modal-dialog form">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title" id="simpleModalLabel">Hapus Data</h4>
+				<h4 class="modal-title text-xl" id="formModalLabel">Hapus Data</h4>
 			</div>
-			<div class="modal-body">
-				<p>Apakah Anda yakin akan menghapus data? Silahkan masukkan password Anda untuk konfirmasi.</p>
+			<div class="modal-body style-default-light">
+				<div class="row">
+					<div class="col-lg-12">
+						<h4 class="text-danger">Perhatian</h4>
+						<p class="opacity-75">
+							Apakah Anda yakin akan menghapus data? Silahkan masukkan password Anda untuk konfirmasi.
+						</p>
+					</div>
+				</div>
 				<div class="row">
 					<div class="form-group">
 						<div class="col-sm-3">
@@ -17,10 +24,10 @@
 					</div>					
 				</div>
 			</div>
-			<div class="modal-footer">
+			<div class="modal-footer style-default-light">
 				<p>{!! Form::hidden('from_confirm_form', 'Yes') !!}</p>
-				<a type="button" class="btn btn-default" data-dismiss="modal">Cancel</a>
-				<button type="submit" type="button" class="btn btn-danger">Hapus</button>
+				<a type="button" class="btn btn-flat" data-dismiss="modal">Cancel</a>
+				<button type="submit" type="button" class="btn btn-flat btn-danger">Hapus</button>
 			</div>
 		</div><!-- /.modal-content -->
 	</div><!-- /.modal-dialog -->
