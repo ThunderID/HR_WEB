d67d9d9491d783eb938e315cea01952ef0373a3d
budi_p <budi-purnomo@outlook.com>
2015-04-29 14:53:43 +0700

    working issue Modal Issue #43 : Kontak dibuat modal event (cannot edit inline, buat seperti work tapi loadnya dari js)

--- resources/views/admin/modals/contact/create.blade.php
+++ resources/views/admin/modals/contact/create.blade.php
@@ -1,9 +1,9 @@
-<div class="modal fade" id="contactCreate" tabindex="-1" role="dialog" aria-labelledby="contactCreate" aria-hidden="true">
+<div class="modal fade modalContact" id="contactCreate" tabindex="-1" role="dialog" aria-labelledby="contactCreate" aria-hidden="true">
 	<div class="modal-dialog form">
 		<div class="modal-content">
 			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-				<h4 class="modal-title text-xl" id="formModalLabel">Tambah Kontak</h4>
+				<h4 class="modal-title text-xl modal_contact_title" id="formModalLabel">Tambah Kontak</h4>
 			</div>
 			<div class="modal-body style-default-light">
 				<div class="row">
@@ -21,13 +21,13 @@
 					<div class="tabs col-md-12  pt-20">
 						<div class="col-md-6">
 							<div class="form-group">
-								<input name="item[1]" id="item[1]" class="form-control getContacts" data-comp="">																
+								<input name="item[1]" id="item[1]" class="form-control getContacts inp_item" data-comp="">																
 								<label for="item[1]">Jenis Kontak</label>
 							</div>
 						</div>
 						<div class="col-md-6">
 							<div class="form-group">
-								<input type="text" class="form-control" id="value[1]" name="value[1]">
+								<input type="text" class="form-control inp_value" id="value[1]" name="value[1]">
 								<label for="value[1]">Isian</label>
 							</div>
 						</div>					
