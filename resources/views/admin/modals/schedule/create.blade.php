5812bfa9a627ee4cbc5db8b2f44e0df2108874c8
uchuy77 <agil.mahendra@gmail.com>
2015-05-07 18:23:30 +0700

    from create schedule modal

---
+++ resources/views/admin/modals/schedule/create.blade.php
@@ -0,0 +1,60 @@
+<div class="modal fade modalSchedule" id="scheduleCreate" tabindex="-1" role="dialog" aria-labelledby="scheduleCreate" aria-hidden="true">
+	<div class="modal-dialog form">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title text-xl modal_contact_title" id="formModalLabel">Tambah Schedule</h4>
+			</div>
+			<div class="modal-body style-default-light">
+				<div class="row">
+					<div class="col-lg-12">
+						<h4 class="text-primary">Petunjuk</h4>
+						<article class="margin-bottom-xxl">
+							<p class="opacity-75">
+								Start merupakan waktu mulai, dan end merupakan waktu berakhir.
+							</p>
+						</article>
+					</div><!--end .col -->
+				</div><!--end .row -->	
+						
+				<div class="row">
+					<div class="tabs col-md-12  pt-20">
+						<div class="col-md-6">
+							<div class="form-group">
+								<input name="item[1]" id="item[1]" class="form-control getName modal_schedule_name" data-comp="">																
+								<label for="item[1]">Nama</label>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" class="form-control modal_schedule_tgl" id="value[1]" name="value[1]">
+								<label for="value[1]">Tanggal</label>
+							</div>
+						</div>					
+					</div>					
+				</div>	
+				<div class="row">
+					<div class="tabs col-md-12  pt-20">
+						<div class="col-md-6">
+							<div class="form-group">
+								<input name="item[1]" id="item[1]" class="form-control modal_schedule_start" data-comp="">																
+								<label for="item[1]">Start</label>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<input type="text" class="form-control modal_schedule_end" id="value[1]" name="value[1]">
+								<label for="value[1]">End</label>
+							</div>
+						</div>					
+					</div>					
+				</div>	
+				<input class="modal_contact_input_id" type="hidden" name="id_item[1]">
+			</div>			
+			<div class="modal-footer style-default-light">
+				<a type="button" class="btn btn-flat" data-dismiss="modal">Batal</a>
+				<button type="submit" type="button" class="btn btn-flat btn-primary modal_schedule_btn_save">Tambah</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div>	
