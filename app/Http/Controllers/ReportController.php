87abaf28b67da4c1621303aca1c70f889e213625
chelsymooy <chelsymooy1108@gmail.com>
2015-06-03 20:04:54 +0700

    ugrade wages to thin controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -471,10 +471,24 @@ class ReportController extends Controller {
 
 		$data 										= json_decode(json_encode($contents->data), true);
 		unset($search);
+
+
+		$search['organisationid'] 					= Session::get('user.organisation');
+		$search['checkwork'] 						= true;
+
+		$results 									= API::person()->index($page, $search, $sort, 100);
+		$contents 									= json_decode($results);
 		
+		if(!$contents->meta->success)
+		{
+			App::abort(404);
+		}
+		$persons 									= json_decode(json_encode($contents->data), true);
+		unset($search);
+
 		$ids 										= [];
 
-		foreach ($data as $key => $value)
+		foreach ($persons as $key => $value)
 		{
 			$ids[] 									= $value['id'];
 		}
@@ -493,23 +507,35 @@ class ReportController extends Controller {
 		$currentstatus 								= [];
 		$compelete 									= [];
 
-		foreach ($data as $key => $value)
+		foreach ($persons as $key => $value)
 		{
 			$minus 									= 0;
+			$quota 									= 0;
+			$plus 									= 0;
 			$status 								= null;
+			foreach ($data as $key1 => $value1)
+			{
+				if($value1['person_id']==$value['id'])
+				{
+					$quota 							= $quota + $value1['quota'];
+					$plus 							= $plus + $value1['plus_quota'];
+				}
+			}
 			foreach ($data2 as $key2 => $value2)
 			{
 				if($value2['person_id']==$value['id'])
 				{
-					$status[$value2['status']] 		= $value2['minus_quota'];
+					$status[$value2['name']] 		= $value2['minus_quota'];
 					$minus 							= $minus + $value2['minus_quota'];
-					if(!in_array($value2['status'], $currentstatus))
+					if(!in_array($value2['name'], $currentstatus))
 					{
-						$currentstatus[] 			= $value2['status'];
+						$currentstatus[] 			= $value2['name'];
 					}
 				}
 			}
 			$compelete[$key] 						= $value;
+			$compelete[$key]['quota'] 				= $quota;
+			$compelete[$key]['plus_quota'] 			= $plus;
 			$compelete[$key]['minus_quota'] 		= $minus;
 			$compelete[$key]['residue_quota'] 		= $compelete[$key]['quota'] + $compelete[$key]['plus_quota'] - $compelete[$key]['minus_quota'];
 			$compelete[$key]['status'] 				= $status;
@@ -531,6 +557,24 @@ class ReportController extends Controller {
 		}
 
 		$branches 									= json_decode(json_encode($contents_2->data), true);
+		if(Input::has('mode'))
+		{
+			Excel::create('Wages Reports', function($excel) use ($compelete, $currentstatus) 
+			{
+				// Set the title
+				$excel->setTitle('Our new awesome title');
+				// Call them separately
+				$excel->setDescription('A demonstration to change the file properties');
+				$excel->sheet('Sheetname', function ($sheet) use ($compelete, $currentstatus) 
+				{
+					$c 								= count($compelete);
+					$cs 							= count($currentstatus);
+					$sheet->setBorder('A1:G'.($c+2), 'thin');
+					$sheet->setWidth(['A' => 5, 'B' => 30, 'C' => 15, 'D' => 15, 'E' => 15, 'F' => 15, 'G' => 15]);
+					$sheet->loadView('admin.pages.reports.wages.wages_csv')->with('data', $compelete)->with('status', $currentstatus)->with('cs', $cs);
+				});
+			})->export(Input::get('mode'));
+		}
 		
 		// ---------------------- GENERATE CONTENT ----------------------
 		$this->layout->page_title 					= ucwords('Generate Wages Report');
@@ -543,154 +587,5 @@ class ReportController extends Controller {
 		$this->layout->content->end 				= $end;
 		
 		return $this->layout;
-	}
-
-	function getWagesCSV($page = null)
-	{
-		// ---------------------- LOAD DATA ----------------------
-		if(Input::has('start'))
-		{
-			list($d,$m,$y) 							= explode('/', Input::get('start'));
-			$start 									= "$y-$m-$d";
-			list($d,$m,$y) 							= explode('/', Input::get('end'));
-			$end 									= "$y-$m-$d";
-			$search 								= ['quotas' => ['ondate'=> [$start, $end]]];
-		}
-		else
-		{
-			$search 								= ['quotas' => ['ondate'=> '']];
-		}
-
-		$sort 										= ['persons.id' => 'desc'];
-
-		if(Input::has('case'))
-		{
-			switch (Input::get('case'))
-			{
-				case 'late':
-				$search['late'] 					= true;
-				$sort 								= ['margin_start' => 'asc'];
-				break;
-
-				case 'ontime':
-				$search['ontime'] 					= true;
-				$sort 								= ['margin_start' => 'desc'];
-				break;
-
-				case 'earlier':
-				$search['earlier']					= true;
-				$sort 								= ['margin_end' => 'asc'];
-				break;
-
-				case 'overtime':
-				$search['overtime']					= true;
-				$sort 								= ['margin_end' => 'desc'];
-				break;
-
-				default:
-				App::abort('404');
-				break;
-			}
-		}
-
-		if(Input::has('branch'))
-		{
-			$search['branchid'] 					= Input::get('branch');
-		}
-
-		if(Input::has('tag'))
-		{
-			$search['charttag'] 					= Input::get('tag');
-		}
-
-		$search['organisationid'] 					= Session::get('user.organisation');
-
-		$results 									= API::person()->index($page, $search, $sort, 100);
-		$contents 									= json_decode($results);
-
-		if(!$contents->meta->success)
-		{
-			App::abort(404);
-		}
-
-		$data 										= json_decode(json_encode($contents->data), true);
-		unset($search);
-
-		foreach ($data as $key => $value)
-		{
-			$ids[] 									= $value['id'];
-		}
-		
-		$search 									= ['minusquotas' => ['ondate'=> [$start, $end], 'ids' => $ids]];
-		$sort 										= ['persons.id' => 'desc'];
-		
-		$results 									= API::person()->index($page, $search, $sort, 100);
-		$contents 									= json_decode($results);
-
-		if(!$contents->meta->success)
-		{
-			App::abort(404);
-		}
-
-		$data2 										= json_decode(json_encode($contents->data), true);
-		$currentstatus 								= [];
-
-		foreach ($data as $key => $value)
-		{
-			$minus 									= 0;
-			$status 								= null;
-
-			foreach ($data2 as $key2 => $value2)
-			{
-				if($value2['person_id']==$value['id'])
-				{
-					$status[$value2['status']]		= $value2['minus_quota'];
-					$minus 							= $minus + $value2['minus_quota'];
-
-					if(!in_array($value2['status'], $currentstatus))
-					{
-						$currentstatus[] 			= $value2['status'];
-					}
-				}
-			}
-
-			$compelete[$key] 						= $value;
-			$compelete[$key]['minus_quota'] 		= $minus;
-			$compelete[$key]['residue_quota'] 		= $compelete[$key]['quota'] + $compelete[$key]['plus_quota'] - $compelete[$key]['minus_quota'];
-			$compelete[$key]['status'] 				= $status;
-		}
-
-		$search 									= ['organisationid' => Session::get('user.organisation')];
-
-		if(Input::has('branch'))
-		{
-			$search['id'] 							= Input::get('branch');
-			$search['DisplayDepartments'] 			= '';
-		}
-
-		$sort 										= ['name' => 'asc'];
-		$results_2 									= API::branch()->index(1, $search, $sort);
-		$contents_2 								= json_decode($results_2);
-
-		if(!$contents_2->meta->success)
-		{
-			App::abort(404);
-		}
-
-		Excel::create('Wages Reports', function($excel) use ($compelete, $currentstatus) 
-		{
-			// Set the title
-			$excel->setTitle('Our new awesome title');
-			// Call them separately
-			$excel->setDescription('A demonstration to change the file properties');
-			$excel->sheet('Sheetname', function ($sheet) use ($compelete, $currentstatus) 
-			{
-				$c 								= count($compelete);
-				$cs 							= count($currentstatus);
-				$sheet->setBorder('A1:G'.($c+2), 'thin');
-				$sheet->setWidth(['A' => 5, 'B' => 30, 'C' => 15, 'D' => 15, 'E' => 15, 'F' => 15, 'G' => 15]);
-				$sheet->loadView('admin.pages.reports.wages.wages_csv')->with('data', $compelete)->with('status', $currentstatus)->with('cs', $cs);
-			});
-		})->export(Input::get('mode'));
 	}
 }
