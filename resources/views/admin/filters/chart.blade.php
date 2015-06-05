c0fcb145370f67749a4501563d1731de401c6145
chelsymooy <chelsymooy1108@gmail.com>
2015-06-05 14:13:12 +0700

    update filter url for chart calendar

--- resources/views/admin/filters/chart.blade.php
+++ resources/views/admin/filters/chart.blade.php
@@ -3,6 +3,6 @@
 	<ul class="nav nav-pills nav-stacked">
 		<li class="text-primary">MENU</li>
 		<li @if(isset($authentications)) class="active" @endif><a href="{{route('hr.branches.charts.show', ['id' => $chart['id'], 'org_id' => $data['id'], 'branchid' => $branch['id']])}}"> Autentikasi </a></li>
-		<li @if(!isset($authentications)) class="active" @endif><a href="{{route('hr.branches.charts.show', ['id' => $chart['id'], 'org_id' => $data['id'], 'branchid' => $branch['id']])}}"> Kalender Kerja </a></li>
+		<li @if(!isset($authentications)) class="active" @endif><a href="{{route('hr.charts.calendars.index', ['id' => $chart['id'], 'org_id' => $data['id'], 'branchid' => $branch['id']])}}"> Kalender Kerja </a></li>
 	</ul>
 </div>
